eca86e709cd8f6b8d5723a37e936ffdeca1ad5ca
eightHundreds <mingoing@outlook.com>
2026-07-08 20:41:51 +0800

    test: 直接打开收藏夹验证同步

--- test/git-tty.test.ts
+++ test/git-tty.test.ts
@@ -1112,9 +1112,7 @@ test('collection browser sync establishes the first upstream and excludes machin
     await exec('git', ['remote', 'add', 'origin', remote], { cwd: collection });
     await run(context, ['import', source, '--all', '--yes']);
 
-    await runInteractive(context, ['list'], [
-      { wait: 'q 退出', send: '\u001b[C', enter: false, delayAfter: 800 },
-      { wait: '全局 ', send: '\u001b[C', enter: false, delayAfter: 200 },
+    await runInteractive(context, [], [
       { wait: 's 同步 Git', send: 's', enter: false, delayAfter: 800 },
       { wait: 'Git 同步完成', send: 'q', enter: false },
     ]);
